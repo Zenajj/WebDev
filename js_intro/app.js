71e7e1c3f3d411dba33a3f559a8e88dc082eed6d
zenaj <zenaj.game@gmail.com>
2021-11-06 18:28:22 +0100

    Added more projects

---
+++ js_intro/app.js
@@ -0,0 +1,18 @@
+// let rand = Math.random();
+
+// if (rand < 0.5) {
+//     console.log("Number is less than 0.5!");
+//     console.log(rand);
+// }
+// if (rand >= 0.5) {
+//     console.log("Number is greater than 0.5!");
+//     console.log(rand);
+// }
+
+const dayOfWeek = "Monday";
+
+if (dayOfWeek === "Monday") {
+    console.log("I hate mondays!");
+} else if (dayOfWeek === "Saturday") {
+    console.log("I like saturdays!");    
+}
